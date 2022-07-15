1736b6d7e3142936d7a90227db83c2e24ac4238e
HTribble <72073496+HTribble@users.noreply.github.com>
2022-07-15 21:25:51 +0100

    added movement improvements

--- RedDeadEstonion-GMTK-2022/Assets/Scripts/PlayerMovement.cs
+++ RedDeadEstonion-GMTK-2022/Assets/Scripts/PlayerMovement.cs
@@ -5,11 +5,16 @@ using UnityEngine.AI;
 
 public class PlayerMovement : MonoBehaviour
 {
-    NavMeshAgent agent;
+    Rigidbody rb;
+    Vector3 targetPos;
+
+    public float speed = 8f;
+    
     // Start is called before the first frame update
     void Start()
     {
-        agent=GetComponent<NavMeshAgent>();
+        rb = GetComponent<Rigidbody>();
+        targetPos = transform.position;
     }
 
     // Update is called once per frame
@@ -17,14 +22,20 @@ public class PlayerMovement : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            RaycastHit hit;
+           RaycastHit hit;
            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
             if (Physics.Raycast(ray, out hit, Mathf.Infinity))
             {
-                agent.SetDestination(hit.point);
-
+                targetPos = hit.point;
             }
         }
-    }   
+    }
+
+    private void FixedUpdate()
+    {
+        if(Vector3.Distance(transform.position, targetPos) > 2)
+            rb.AddForce(targetPos.normalized * speed);
+
+    }
 }
