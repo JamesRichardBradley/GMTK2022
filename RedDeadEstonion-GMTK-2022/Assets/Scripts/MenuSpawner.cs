d6945516615ab0b4285337392b82b45cb0ba0923
chiaracarrozza <90903541+chiaracarrozza@users.noreply.github.com>
2022-07-16 21:28:07 +0100

    roulettes

--- RedDeadEstonion-GMTK-2022/Assets/Scripts/MenuSpawner.cs
+++ RedDeadEstonion-GMTK-2022/Assets/Scripts/MenuSpawner.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class MenuSpawner : MonoBehaviour
 {
+    [SerializeField] GameObject[] Spawnees;
+
+    [SerializeField]
+    bool stopSpawning;
+
+    [SerializeField]
+    float spawnTime;
+
+    [SerializeField]
+    float spawndelay;
+
+    [SerializeField]
+    float spawnRange;
     // Start is called before the first frame update
     void Start()
     {
