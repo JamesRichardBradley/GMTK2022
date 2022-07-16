e4fed9bc56daa0921c023664e1dbe7ffcb8325af
gsinghbassi <100673uk@saeinstitute.edu>
2022-07-16 14:06:44 +0100

    cheeky

--- RedDeadEstonion-GMTK-2022/Assets/Scripts/CameraController.cs
+++ RedDeadEstonion-GMTK-2022/Assets/Scripts/CameraController.cs
@@ -7,6 +7,7 @@ public class CameraController : MonoBehaviour
     Transform target;
     Vector3 velocity;
     float yaw;
+    float pitch;
 
     public Vector3 offset = new Vector3(0f, 3f, 5f);
     public float smoothDamp = 0.5f;
@@ -17,21 +18,29 @@ public class CameraController : MonoBehaviour
     void Start()
     {
         target = FindObjectOfType<PlayerMovement>().transform;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
         yaw += Input.GetAxis("Mouse X") * sensitivity;
+        pitch += Input.GetAxis("Mouse Y") * sensitivity;
 
-        Vector3 pos = (target.position + offset);
+        pitch = Mathf.Clamp(pitch, -45, 45);
+        Quaternion input = Quaternion.Euler(pitch, yaw, 0);
 
-        transform.position = Vector3.SmoothDamp(transform.position, pos, ref velocity, smoothDamp);
+        Quaternion targetRotation = Quaternion.LookRotation((target.transform.position + transform.forward) - transform.position);
 
-
-        var targetRotation = Quaternion.LookRotation((target.transform.position + transform.forward) - transform.position);
+        Quaternion overallRot = Quaternion.FromToRotation(targetRotation.eulerAngles, input.eulerAngles);
 
         // Smoothly rotate towards the target point.
-        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, smoothLook * Time.deltaTime);
+        transform.rotation = Quaternion.Slerp(transform.rotation, overallRot, smoothLook * Time.deltaTime);
+
+
+        Vector3 pos = (target.position + offset);
+        transform.position = Vector3.SmoothDamp(transform.position, pos, ref velocity, smoothDamp);
+
     }
 }
