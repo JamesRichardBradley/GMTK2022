9a99a8a07105a04e840867494524a7d5f644a993
gsinghbassi <100673uk@saeinstitute.edu>
2022-07-16 17:47:38 +0100

    Added Die Function

--- RedDeadEstonion-GMTK-2022/Assets/Scripts/PlayerManager.cs
+++ RedDeadEstonion-GMTK-2022/Assets/Scripts/PlayerManager.cs
@@ -4,24 +4,29 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
-   
-    public float forwardAngle;
 
-    private void Update()
-    {
-        forwardAngle = Vector3.Angle(transform.forward, Vector3.up);
-
-    }
 
     int DieFacing()
     {
         int die = 0;
 
-        forwardAngle = Vector3.Angle(transform.forward,Vector3.up);
+        float forwardAngle = Vector3.Angle(transform.forward,Vector3.up);
         if(forwardAngle < 20)
             die = 1;
         if (forwardAngle > 160 && forwardAngle < 200)
             die = 5;
+
+        float rightAngle = Vector3.Angle(transform.right, Vector3.up);
+        if (rightAngle < 20)
+            die = 2;
+        if (rightAngle > 160 && rightAngle < 200)
+            die = 4;
+
+        float upAngle = Vector3.Angle(transform.up, Vector3.up);
+        if (upAngle < 20)
+            die = 6;
+        if (upAngle > 160 && upAngle < 200)
+            die = 3;
 
         return die;
     }
@@ -29,5 +34,21 @@ public class PlayerManager : MonoBehaviour
     private void OnCollisionStay(Collision collision)
     {
         print(DieFacing());
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, transform.right * 3);
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawRay(transform.position, transform.up * 3);
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawRay(transform.position, transform.forward * 3);
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawRay(transform.position, Vector3.up * 3);
+
     }
 }
