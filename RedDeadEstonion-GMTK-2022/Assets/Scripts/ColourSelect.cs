dd773ab3c7b5c55891b612a5ba5652206fcdd9f3
JamesRichardBradley <99551uk@saeinstitute.edu>
2022-07-16 15:19:33 +0100

    Barrier Path

---
+++ RedDeadEstonion-GMTK-2022/Assets/Scripts/ColourSelect.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColourSelect : MonoBehaviour
+{
+    public GameObject[] chips;
+    private int selection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(var chip in chips)
+        {
+            selection = Random.Range(0, 10);
+            var chipRenderer = chip.GetComponent<Renderer>();
+
+            if (selection <= 5)
+            {
+                chipRenderer.material.SetColor("_Color", Color.red);
+            }
+            else
+            {
+             chipRenderer.material.SetColor("_Color", Color.black);
+            }
+        }
+    }
+}
