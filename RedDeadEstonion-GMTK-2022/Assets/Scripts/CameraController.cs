6a986d0470f9fe85fe8d9f4e0adab94276779aae
gsinghbassi <100673uk@saeinstitute.edu>
2022-07-16 13:38:36 +0100

    oi oi salvaloy

--- RedDeadEstonion-GMTK-2022/Assets/Scripts/CameraController.cs
+++ RedDeadEstonion-GMTK-2022/Assets/Scripts/CameraController.cs
@@ -6,6 +6,7 @@ public class CameraController : MonoBehaviour
 {
     Transform target;
     Vector3 velocity;
+    float yaw;
 
     public Vector3 offset = new Vector3(0f, 3f, 5f);
     public float smoothDamp = 0.5f;
@@ -21,13 +22,14 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        float horizontal = Input.GetAxis("Mouse X") * sensitivity;
+        yaw += Input.GetAxis("Mouse X") * sensitivity;
+
+        Vector3 pos = (target.position + offset);
+
+        transform.position = Vector3.SmoothDamp(transform.position, pos, ref velocity, smoothDamp);
 
 
-
-        transform.position = Vector3.SmoothDamp(transform.position, target.position + offset, ref velocity, smoothDamp);
-
-        var targetRotation = Quaternion.LookRotation((target.transform.position) - transform.position);
+        var targetRotation = Quaternion.LookRotation((target.transform.position + transform.forward) - transform.position);
 
         // Smoothly rotate towards the target point.
         transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, smoothLook * Time.deltaTime);
